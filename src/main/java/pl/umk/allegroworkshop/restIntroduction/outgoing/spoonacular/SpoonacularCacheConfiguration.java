82afa64b0b4f7db738d37a5bab01c3fe44d58a7b
Mateusz Wasieczko <mateusz.wasieczko@allegro.pl>
2022-12-06 02:17:03 +0100

    imports

--- src/main/java/pl/umk/allegroworkshop/restIntroduction/outgoing/spoonacular/SpoonacularCacheConfiguration.java
+++ src/main/java/pl/umk/allegroworkshop/restIntroduction/outgoing/spoonacular/SpoonacularCacheConfiguration.java
@@ -3,14 +3,10 @@ package pl.umk.allegroworkshop.restIntroduction.outgoing.spoonacular;
 import com.github.benmanes.caffeine.cache.Caffeine;
 import com.github.benmanes.caffeine.cache.LoadingCache;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.autoconfigure.cache.CacheProperties;
-import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.client.RestTemplate;
 import pl.umk.allegroworkshop.restIntroduction.outgoing.spoonacular.model.SpoonacularIngredientInformation;
 
-import java.time.Duration;
 import java.util.concurrent.TimeUnit;
 
 @Configuration
