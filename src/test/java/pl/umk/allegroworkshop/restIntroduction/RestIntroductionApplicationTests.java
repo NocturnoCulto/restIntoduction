37602d8ee5de2c3db6db0cc513b3089783394bd3
Mateusz Wasieczko <mateusz.wasieczko@allegro.pl>
2023-03-14 01:05:01 +0100

    work in progress

--- src/test/java/pl/umk/allegroworkshop/restIntroduction/RestIntroductionApplicationTests.java
+++ src/test/java/pl/umk/allegroworkshop/restIntroduction/RestIntroductionApplicationTests.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import pl.umk.allegroworkshop.restIntroduction.api.v1.MealsApi;
-import pl.umk.allegroworkshop.restIntroduction.api.v1.model.response.*;
 import pl.umk.allegroworkshop.restIntroduction.domain.model.ExternalIngredientsList;
 
 
@@ -14,33 +13,7 @@ import static org.junit.jupiter.api.Assertions.*;
 class RestIntroductionApplicationTests extends BaseTest {
 
 	@Test
-	void getAllBooksEndpoint() throws Exception {
-		String uri = "/getMeals";
-		MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.get(uri)
-				.accept(MealsApi.apiVersionAccept)).andReturn();
-
-		int status = mvcResult.getResponse().getStatus();
-		assertEquals(200, status);
-		String content = mvcResult.getResponse().getContentAsString();
-		MealsResponse meals = super.mapFromJson(content, MealsResponse.class);
-		assertTrue(meals.getMeals() .size() > 0);
-	}
-
-	@Test
-	void getOneBookEndpoint() throws Exception {
-		String uri = "/getMeal/125";
-		MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.get(uri)
-				.accept(MealsApi.apiVersionAccept)).andReturn();
-
-		int status = mvcResult.getResponse().getStatus();
-		assertEquals(200, status);
-		String content = mvcResult.getResponse().getContentAsString();
-		MealsResponse meals = super.mapFromJson(content, MealsResponse.class);
-		assertEquals(1, meals.getMeals().size());
-	}
-
-	@Test
-	void getFromService() throws Exception {
+	void shouldSearchIngredientsByName() throws Exception {
 		String uri = "/searchIngredients?name=banana";
 		MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.get(uri)
 				.accept(MealsApi.apiVersionAccept)).andReturn();
