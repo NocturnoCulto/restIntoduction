b798fb547adb160d72c607ca370577751eade3ba
Mateusz Wasieczko <mateusz.wasieczko@allegro.pl>
2023-03-14 01:28:25 +0100

    zadania

--- src/test/java/pl/umk/allegroworkshop/restIntroduction/RestIntroductionApplicationTests.java
+++ src/test/java/pl/umk/allegroworkshop/restIntroduction/RestIntroductionApplicationTests.java
@@ -1,27 +1,53 @@
 package pl.umk.allegroworkshop.restIntroduction;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import pl.umk.allegroworkshop.restIntroduction.api.v1.MealsApi;
+import pl.umk.allegroworkshop.restIntroduction.api.v1.model.response.MealsResponse;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class RestIntroductionApplicationTests extends BaseTest {
 
-	@Test
-	void shouldSearchIngredientsByName() throws Exception {
+    @Test
+    void shouldReturnAllMeals() throws Exception {
+        //given:
+        String uri = "/getMeals";
 
-	}
+        //when:
+        MvcResult mvcResult = mvc.perform(MockMvcRequestBuilders.get(uri)
+                .accept(MealsApi.apiVersionAccept)).andReturn();
 
-	@Test
-	void shouldSearchIngredientsDetails() throws Exception {
+        //then:
+        int status = mvcResult.getResponse().getStatus();
+        assertEquals(200, status);
+        String content = mvcResult.getResponse().getContentAsString();
+        MealsResponse meals = super.mapFromJson(content, MealsResponse.class);
+        assertEquals(4, meals.getMeals().size());
+    }
 
-	}
+    @Test
+    void shouldSearchIngredientsByName() throws Exception {
+        assertEquals(true, false);
+    }
 
-	@Test
-	void shouldCachedIngredientsDetails() throws Exception {
+    @Test
+    void shouldSearchIngredientsDetails() throws Exception {
+        assertEquals(true, false);
 
-	}
+    }
 
-	@Test
-	void shouldCatchSpoonacularExceptions() throws Exception {
+    @Test
+    void shouldCachedIngredientsDetails() throws Exception {
+        assertEquals(true, false);
 
-	}
+    }
+
+    @Test
+    void shouldCatchSpoonacularExceptions() throws Exception {
+        assertEquals(true, false);
+
+    }
 
 }
