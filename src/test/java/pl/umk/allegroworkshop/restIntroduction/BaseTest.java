77dba08ad3cf7cbf567c36dfddfc87b8c653ee41
Mateusz Wasieczko <mateusz.wasieczko@allegro.pl>
2023-03-27 22:28:24 +0200

    test placeholders

--- src/test/java/pl/umk/allegroworkshop/restIntroduction/BaseTest.java
+++ src/test/java/pl/umk/allegroworkshop/restIntroduction/BaseTest.java
@@ -1,7 +1,6 @@
 package pl.umk.allegroworkshop.restIntroduction;
 
 import com.fasterxml.jackson.core.JsonParseException;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
