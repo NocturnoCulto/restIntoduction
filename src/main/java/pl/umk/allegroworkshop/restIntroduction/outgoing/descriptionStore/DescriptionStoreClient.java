b6eb3435ce97a50f17f44fd6e24e31d948c21e31
Mateusz Wasieczko <mateusz.wasieczko@allegro.pl>
2024-03-07 03:05:15 +0100

    start zajec

--- src/main/java/pl/umk/allegroworkshop/restIntroduction/outgoing/descriptionStore/DescriptionStoreClient.java
+++ src/main/java/pl/umk/allegroworkshop/restIntroduction/outgoing/descriptionStore/DescriptionStoreClient.java
@@ -2,41 +2,14 @@ package pl.umk.allegroworkshop.restIntroduction.outgoing.descriptionStore;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.stereotype.Component;
-import org.springframework.web.client.RestTemplate;
-import org.springframework.web.util.UriComponentsBuilder;
 import pl.umk.allegroworkshop.restIntroduction.outgoing.descriptionStore.model.ExternalDescription;
 
-@Component
 public class DescriptionStoreClient {
-    private final RestTemplate descriptionStoreRestTemplate;
-    private final RestTemplate descriptionStoreRestTemplateForRetry;
-    private final DescriptionStoreClientConfiguration configuration;
 
     private final Logger logger = LoggerFactory.getLogger(DescriptionStoreClient.class);
 
-    public DescriptionStoreClient(RestTemplate descriptionStoreRestTemplate, RestTemplate descriptionStoreRestTemplateForRetry, DescriptionStoreClientConfiguration configuration) {
-        this.descriptionStoreRestTemplate = descriptionStoreRestTemplate;
-        this.descriptionStoreRestTemplateForRetry = descriptionStoreRestTemplateForRetry;
-        this.configuration = configuration;
-    }
 
     public ExternalDescription findDescriptionById(String id) {
-        String uriString = UriComponentsBuilder.fromUriString(configuration.getUrl())
-                .path(String.format(configuration.getPath(), id))
-                .build()
-                .toUriString();
-
-        try {
-            return descriptionStoreRestTemplate.getForObject(uriString, ExternalDescription.class);
-        } catch (Exception ex) {
-            try {
-                logger.warn("Retry request for description id={}. Exception for first fail = {}", id, ex.getMessage());
-                return descriptionStoreRestTemplateForRetry.getForObject(uriString, ExternalDescription.class);
-            } catch (Exception ex2) {
-                logger.error("Second request for description id={} failed. Exception = {}", id, ex.getMessage());
-                return null;
-            }
-        }
+        return null;
     }
 }
--- src/main/java/pl/umk/allegroworkshop/restIntroduction/outgoing/descriptionStore/DescriptionStoreClientConfiguration.java
+++ src/main/java/pl/umk/allegroworkshop/restIntroduction/outgoing/descriptionStore/DescriptionStoreClientConfiguration.java
@@ -1,14 +1,7 @@
 package pl.umk.allegroworkshop.restIntroduction.outgoing.descriptionStore;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.client.RestTemplate;
 
-import java.time.Duration;
-
-@Configuration
 public class DescriptionStoreClientConfiguration {
     private final String url;
     private final String path;
@@ -16,11 +9,11 @@ public class DescriptionStoreClientConfiguration {
     private final Long readTimeout;
     private final Long retryReadTimeout;
 
-    public DescriptionStoreClientConfiguration(@Value("${description_store.url}") String url,
-                                               @Value("${description_store.path}") String path,
-                                               @Value("${description_store.connect_timeout}") Long connectTimeout,
-                                               @Value("${description_store.read_timeout}") Long readTimeout,
-                                               @Value("${description_store.retry_read_timeout}") Long retryReadTimeout) {
+    public DescriptionStoreClientConfiguration(String url,
+                                               String path,
+                                               Long connectTimeout,
+                                               Long readTimeout,
+                                               Long retryReadTimeout) {
         this.url = url;
         this.path = path;
         this.connectTimeout = connectTimeout;
@@ -28,39 +21,4 @@ public class DescriptionStoreClientConfiguration {
         this.retryReadTimeout = retryReadTimeout;
     }
 
-    @Bean
-    public RestTemplate descriptionStoreRestTemplate() {
-        return new RestTemplateBuilder()
-                .setConnectTimeout(Duration.ofMillis(connectTimeout))
-                .setReadTimeout(Duration.ofMillis(readTimeout))
-                .build();
-    }
-
-    @Bean
-    public RestTemplate descriptionStoreRestTemplateForRetry() {
-        return new RestTemplateBuilder()
-                .setConnectTimeout(Duration.ofMillis(connectTimeout))
-                .setReadTimeout(Duration.ofMillis(retryReadTimeout))
-                .build();
-    }
-
-    public String getUrl() {
-        return url;
-    }
-
-    public String getPath() {
-        return path;
-    }
-
-    public Long getConnectTimeout() {
-        return connectTimeout;
-    }
-
-    public Long getReadTimeout() {
-        return readTimeout;
-    }
-
-    public Long getRetryReadTimeout() {
-        return retryReadTimeout;
-    }
 }
